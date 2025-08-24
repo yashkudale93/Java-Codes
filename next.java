1d1f83a08651aeb2613e10e98be2fd0e4fca585f
Yash kudale <yashkudale93@gmail.com>
2025-08-24 22:19:22 +0530

    Create next.java

---
+++ next.java
@@ -0,0 +1,8 @@
+public class next {
+    public static void main(String[] args) {
+        System.out.println("Hello world with java");
+        System.out.println("Hello world with java");
+        System.out.print("Hello world with java");
+    }
+    
+}
