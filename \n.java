5eb10bc44d307c0ddbb06e01c51cfeb37b310662
Yash kudale <yashkudale93@gmail.com>
2025-08-25 12:24:21 +0530

    Add "\n"program in Java

---
+++ \n.java
@@ -0,0 +1,8 @@
+public class next1 {
+    public static void main(String[] args) {
+        System.out.print("Hello world with java\n");
+        System.out.print("Hello world with java\n");
+        System.out.print("Hello world with java");
+    }
+    
+}
