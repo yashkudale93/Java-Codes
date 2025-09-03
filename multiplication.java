0addda06484e94bd4f79ffe2331a4dfc7f9f7126
Yash kudale <yashkudale93@gmail.com>
2025-09-03 17:20:48 +0530

    Implement multiplication program in Java (a*b)

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+public class mul {
+    public static void main(String[] args) {
+        //Variables
+        int a = 10;
+        int b = 25;
+        int sum = a+b;
+       // System.out.println(sum);  
+
+       int mul = a * b;
+       System.out.println(mul);
+       
+
+    
+}
+    
+}
