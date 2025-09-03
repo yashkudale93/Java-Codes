b32d463cffad83ebb599a7c9b348890db3c27447
Yash kudale <yashkudale93@gmail.com>
2025-09-03 17:51:36 +0530

    Implement divide program in Java (a/b)

---
+++ divide.java
@@ -0,0 +1,16 @@
+public class divide {
+    public static void main(String[] args) {
+        //Variables
+        int a = 7;
+        int b = 3;
+        int sum = a+b;
+       // System.out.println(sum);  
+
+       int divide = a/b;
+       System.out.println(divide);
+       
+
+    
+}
+    
+}
